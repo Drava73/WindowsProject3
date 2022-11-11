047e9fb355d823db61987c79d5d2269bd07aeaa2
My Name <kiykobogdan@gmail.com>
2022-11-11 22:32:57 +0200

    дz

--- WindowsProject3/WindowsProject3.cpp
+++ WindowsProject3/WindowsProject3.cpp
@@ -1,93 +1,109 @@
-#include <Windows.h>
-#include <tchar.h>
-int count_lbutton = 0;  
-int count_rbutton = 0;  
-int cout_mbutton = 0;  
-char info[4096];  
+#include<Windows.h>
+#include<tchar.h>
+
+namespace glb {
+	int margin = 10;
+	TCHAR str[128], inf[128], winInfo[128];
+	INT x, y, width, height;
+}
+
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 
-TCHAR szClassWindow[] = TEXT("Каркасное приложение");
+TCHAR szClassWindow[] = TEXT("приложение");   
 
-INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine,
-    int nCmdShow)
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
 {
+	HWND hWnd;
+	MSG lpMsg;
+	WNDCLASSEX wcl;
 
-    HWND hWnd;
-    MSG lpMsg;
-    WNDCLASSEX wcl;
-     
-    wcl.cbSize = sizeof(wcl); 
-    wcl.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS; 
-    wcl.lpfnWndProc = WindowProc;
-    wcl.cbClsExtra = 0;
-    wcl.cbWndExtra = 0;
-    wcl.hInstance = hInst;
-    wcl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wcl.hCursor = LoadCursor(NULL, IDC_HAND);
-    wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-    wcl.lpszMenuName = NULL;
-    wcl.lpszClassName = szClassWindow;
-    wcl.hIconSm = NULL;
-    
-    if (!RegisterClassEx(&wcl))
-        return 0;
-      
-    hWnd = CreateWindowEx(
-        0,
-        szClassWindow,
-        TEXT("Каркас приложение Windows"),
-        WS_OVERLAPPEDWINDOW,
-        200,
-        200,
-        500,
-        500,
-        NULL,
-        NULL,
-        hInst,
-        NULL);
-    
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-    while (GetMessage(&lpMsg, NULL, 0, 0))
-    {
-        TranslateMessage(&lpMsg);
-        DispatchMessage(&lpMsg);
-    }
+	 
+	wcl.cbSize = sizeof(wcl);
+	wcl.style = CS_HREDRAW | CS_VREDRAW;
+	wcl.lpfnWndProc = WindowProc;
+	wcl.cbClsExtra = 0;
+	wcl.cbWndExtra = 0;
+	wcl.hInstance = hInst;
+	wcl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcl.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcl.lpszMenuName = NULL;
+	wcl.lpszClassName = szClassWindow;
+	wcl.hIconSm = NULL;
 
-    return 0;
+	 
+	if (!RegisterClassEx(&wcl))
+		return 0;
+
+	 
+	hWnd = CreateWindowEx(
+		0,
+		szClassWindow,
+		TEXT("My second window"),
+		WS_OVERLAPPEDWINDOW,
+		800,
+		200,
+		500,
+		300,
+		NULL,
+		NULL,
+		hInst,
+		NULL
+	);
+
+	/* 4. Отображение окна */
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	/* 5. Запуск цикла обработки сooбщений */
+	while (GetMessage(&lpMsg, NULL, 0, 0))
+	{
+		TranslateMessage(&lpMsg);
+		DispatchMessage(&lpMsg);
+	}
+	return lpMsg.wParam;
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
-    switch (uMessage)
-    {
-    case WM_LBUTTONDOWN:
-        count_lbutton++;  
-        MessageBox(0, TEXT("Нажата левая кнопка мыши"), TEXT("WM_LBUTTON"), MB_OK | MB_ICONINFORMATION);
-        sprintf(info, "lbutton: %d rbutton: %d mbutton: %d", count_lbutton, count_rbutton, cout_mbutton);  
-        SetWindowText(hWnd, info);  
+	RECT clientRect;
+	GetClientRect(hWnd, &clientRect);		 
 
-        break;
-    case WM_RBUTTONDOWN:
-        count_rbutton++;
-        MessageBox(0, TEXT("нажата правая кнопка мыши"), TEXT("WM_RBUTTON"), MB_OK | MB_ICONINFORMATION);
-        sprintf(info, "lbutton: %d rbutton: %d mbutton: %d", count_lbutton, count_rbutton, cout_mbutton);
-        SetWindowText(hWnd, info);
-        break;
-    case WM_MBUTTONDOWN:
-        cout_mbutton++;
-        MessageBox(0, TEXT("Нажата средняя кнопка мыши"), TEXT("WM_MBUTTON"), MB_OK | MB_ICONINFORMATION);
-        sprintf(info, "lbutton: %d rbutton: %d mbutton: %d", count_lbutton, count_rbutton, cout_mbutton);
-        SetWindowText(hWnd, info);
-        break;
-    case WM_DESTROY:
-        sprintf(info, "lbutton: %d rbutton: %d mbutton: %d", count_lbutton, count_rbutton, cout_mbutton);
-        MessageBox(0, info, "LOG", 0);  
-        PostQuitMessage(0);
-        break;
+	switch (uMessage)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
 
-    default:
-        return DefWindowProc(hWnd, uMessage, wParam, lParam);
-    }
-    return 0;
+	case WM_LBUTTONDOWN:
+
+		glb::x = LOWORD(lParam);					 
+		glb::y = HIWORD(lParam);					 
+
+		if (glb::x > glb::margin && glb::x < clientRect.right - glb::margin && glb::y > glb::margin && glb::y < clientRect.bottom - glb::margin)
+			wcscpy_s(glb::inf, TEXT("IN"));
+
+		else if (glb::x < glb::margin || glb::x > clientRect.right - glb::margin || glb::y < glb::margin || glb::y > clientRect.bottom - glb::margin)
+			wcscpy_s(glb::inf, TEXT("OUT"));
+
+		else if (glb::x == glb::margin && glb::y > glb::margin && glb::y < clientRect.bottom - glb::margin
+			|| glb::x == clientRect.right - glb::margin && glb::y > glb::margin && glb::y < clientRect.bottom - glb::margin
+			|| glb::y == glb::margin && glb::x > glb::margin && glb::x < clientRect.right - glb::margin
+			|| glb::y == clientRect.bottom - glb::margin && glb::x > glb::margin && glb::x < clientRect.right - glb::margin)
+			wcscpy_s(glb::inf, TEXT("BORDER"));
+
+		wsprintf(glb::str, TEXT("%s"), glb::inf);
+		SetWindowText(hWnd, glb::str);			 
+
+	case WM_RBUTTONDOWN:
+		glb::width = clientRect.right;
+		glb::height = clientRect.bottom;
+		wsprintf(glb::winInfo, TEXT("width: %d  height: %d"), glb::width, glb::height);
+		SetWindowText(hWnd, glb::winInfo);			 
+		break;
+
+	default:
+		return DefWindowProc(hWnd, uMessage, wParam, lParam);
+	}
+	return 0;
 }
